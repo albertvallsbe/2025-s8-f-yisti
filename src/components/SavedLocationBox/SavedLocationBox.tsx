8c9d7e161ad30426212811f611b0f9d4a1b44134
GerardAsuncion <asunciongerard90@gmail.com>
2025-10-10 15:47:40 +0200

    moved everything from component Map.tsx to MapPage.tsx, created LocationsPage and component SavedLocationBox, fixed error in SearchBox it wasn't marking the map location

---
+++ src/components/SavedLocationBox/SavedLocationBox.tsx
@@ -0,0 +1,9 @@
+
+
+export const SavedLocationBox = () => {
+	return (
+		<div>
+			hello
+		</div>
+	)
+}
---
+++ src/pages/LocationsPage/LocationsPage.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../../components/Layout/Layout"
+import { SavedLocationBox } from "../../components/SavedLocationBox/SavedLocationBox"
+
+export const LocationsPage = () => {
+	return (
+			<Layout>
+				<SavedLocationBox />
+			</Layout>
+	)
+}
